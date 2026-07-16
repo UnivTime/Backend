c0ac8bdd93136dc0c5376adb80eb287dc57135a9
김민호 <2022305011@skuniv.ac.kr>
2026-07-16 11:13:38 +0900

    :tada: Start: Begin a new project

---
+++ src/main/java/com/UnivTime/UnivTimeApplication.java
@@ -0,0 +1,13 @@
+package com.UnivTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UnivTime/UnivTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UnivTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
